4ef8ffd3fc7e6a0611b90193575d599216db7977
Bita Zamani <bitazamany@gmail.com>
2024-08-25 23:04:41 +0330

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,44 @@ import "./components/fonts.css";
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import ChristianCal from "./components/christianCal";
 import SolarCal from "./components/solarCal";
-import CurrentDate from "./components/currentDate";
+import { useEffect } from "react";
+import { useState } from "react";
 const App = () => {
+  const [persianDay, setPersianDay] = useState("");
+  const [persianMonth, setPersianMonth] = useState("");
+  const [persianYear, setPersianYear] = useState("");
+  const [persianWeekDay, setPersianWeekDay] = useState("");
+  const [englishDay, setEnglishDay] = useState("");
+  const [englishMonth, setEnglishMonth] = useState("");
+  const [englishYear, setEnglishYear] = useState("");
+  const [englishWeekDay, setEnglishWeekDay] = useState("");
+  useEffect(() => {
+    const date = new Date();
+    setEnglishDay(date.toLocaleDateString("en-us", { day: "numeric" }));
+    setEnglishMonth(date.toLocaleDateString("en-us", { month: "long" }));
+    setEnglishYear(date.getFullYear());
+    setEnglishWeekDay(
+      date.toLocaleDateString("en-us", {
+        weekday: "long",
+      })
+    );
+    setPersianDay(
+      date.toLocaleDateString("fa-IR-u-cu-persian", { day: "numeric" })
+    );
+    setPersianMonth(
+      date.toLocaleDateString("fa-IR-u-cu-persian", { month: "long" })
+    );
+    setPersianYear(
+      date.toLocaleDateString("fa-IR-u-cu-persian", {
+        year: "numeric",
+      })
+    );
+    setPersianWeekDay(
+      date.toLocaleDateString("fa-IR-u-cu-persian", {
+        weekday: "long",
+      })
+    );
+  }, []);
   return (
     <div className="grid-container">
       <Router>
@@ -25,10 +61,20 @@ const App = () => {
 
         <main className="main-section">
           <Routes>
-            <Route path="/age-calculator" element={<CurrentDate />} />
+            <Route path="/" element={<App />} />
             <Route path="/solarCal" element={<SolarCal />} />
             <Route path="/christianCal" element={<ChristianCal />} />
           </Routes>
+          <main className="date-container">
+            <p className="vazirmatn-regular">
+              {persianWeekDay} {persianDay} {persianMonth} {persianYear}
+            </p>
+
+            <p className="poppins-regular">
+              {englishWeekDay} {englishMonth} {englishDay}, {englishYear}
+            </p>
+          </main>
+          );
         </main>
       </Router>
     </div>
